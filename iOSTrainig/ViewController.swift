b4a9870fb4872dd1996916e4945ef22845a3b9f8
FDC-Kit <fdc.christiankit@gmail.com>
2025-03-03 10:11:46 +0800

    Initial Commit

---
+++ iOSTrainig/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSTrainig
+//
+//  Created by FDC-Kit on 3/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
